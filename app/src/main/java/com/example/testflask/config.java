cea240c8d6da45e3fcba5d2ace2282f8d67270a4
ariqhadi <ariq.hadi@gmail.com>
2019-09-05 16:19:15 +0700

    first commit

---
+++ app/src/main/java/com/example/testflask/config.java
@@ -0,0 +1,8 @@
+package com.example.testflask;
+
+public class config {
+
+    public static final String BASE_URL = "http://153.92.4.72:5000/";
+    public static final String API_DIR = "/";
+    public static final String API_UPLOAD = BASE_URL ;
+}
